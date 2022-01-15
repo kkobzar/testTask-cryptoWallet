350cb838e1e8c39299317af8497470be34c49abc
Kiril Kobzar <kirilkobzar@gmail.com>
2022-01-15 12:48:19 +0100

    Wallets assets conversion to USD

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -19,6 +19,7 @@
           <div class="card-body">
             <h5 class="card-title">Your balance</h5>
             <p class="card-text">{{wallets[coin]}} {{coin}}</p>
+            <p class="card-text" v-if="walletsConverted[coin] >= 0">{{walletsConverted[coin]}} USD</p>
           </div>
         </div>
       </div>
@@ -37,29 +38,39 @@ export default {
         ETH:1.2,
         USDT:1600
       },
+      walletsConverted:{
+        BTC:-1,
+        ETH:-1,
+        USDT:-1
+      },
       availableCoins:['BTC','ETH','USDT'],
-      selectedCoin:'BTC',
       connection:null
     }
   },
-  methods:{
-    selectCoin(coin){
-      this.$data.selectedCoin = coin
-    }
-  },
   created() {
-    /*this.$data.connection = new WebSocket('wss://streamer.cryptocompare.com/v2?api_key=559502082285f2c30ccf32d815bec78b3d57fd92637518f8b49567cdfa19af0d')
+    this.$data.connection = new WebSocket('wss://streamer.cryptocompare.com/v2?api_key=559502082285f2c30ccf32d815bec78b3d57fd92637518f8b49567cdfa19af0d')
     this.$data.connection.onopen = (event)=>{
       console.log('Connected to WS')
       console.log(event)
       this.$data.connection.send(JSON.stringify({
         "action": "SubAdd",
-        "subs": ["2~Coinbase~BTC~USD"]
+        "subs": ["2~Coinbase~BTC~USD", "2~Coinbase~ETH~USD", "2~Coinbase~USDT~USD"]
       }))
     }
     this.$data.connection.onmessage = (event) =>{
-      console.log(JSON.parse(event.data))
-    }*/
+      //console.log(JSON.parse(event.data))
+      const data = JSON.parse(event.data)
+      if (data.TYPE === "2"){
+        console.log(data)
+        if (typeof this.$data.walletsConverted[data.FROMSYMBOL] !== 'undefined' && typeof this.$data.wallets[data.FROMSYMBOL] !== 'undefined' && typeof data.PRICE !== 'undefined'){
+          this.$data.walletsConverted[data.FROMSYMBOL] = data.PRICE * this.$data.wallets[data.FROMSYMBOL]
+          this.$data.walletsConverted[data.FROMSYMBOL] = this.$data.walletsConverted[data.FROMSYMBOL].toFixed(2)
+        }
+      }
+    }
+  },
+  unmounted() {
+    this.$data.connection.close()
   }
 }
 </script>
